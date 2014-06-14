4972929519e332119fe34b8150b0200982bf5b78
ueki yoshihiro <pirori67@gmail.com>
2014-06-14 21:24:31 +0900

    練習問題まで醜虜う

---
+++ captialize.rb
@@ -0,0 +1,29 @@
+#練習問題（3）
+
+def word_capitalize(str)
+  str.gsub(/(\w+)/){ |v| v.capitalize }
+end
+ 
+
+p word_capitalize('in-reply-to')
+p word_capitalize('X-MAILER')
+=begin
+def word_capitalize(str)
+	nstr=str.gsub(/^.?/) do |matched|
+		print matched.upcase
+	end
+	print nstr + "\n"
+
+	str.gsub(/-.+?-/) do |matched|
+		/-(.)(.+)/ =~ matched
+		print $1.upcase + $2 + "\n"
+	end
+
+	str.gsub(/-.+?$/) do |matched|
+		/-(.)(.+)/ =~ matched
+		print $1.upcase + $2 + "\n"
+	end	
+end
+
+word_capitalize("in-reply-to")
+=end
---
+++ regexp-minmach.rb
@@ -0,0 +1,3 @@
+p /A.*F/ =~ "ABC DEF"
+p /A.*?F/ =~ "ABCF\"tDEF"
+p /A.+?B/ =~ "ABABABABAB"
