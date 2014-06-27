09fb232bdca140b43e71b6906e319a4606faf97e
ueki yoshihiro <pirori67@gmail.com>
2014-06-27 16:49:57 +0900

    hashのテスト

--- iterater.rb
+++ iterater.rb
@@ -7,3 +7,26 @@ end
 
 value=["Ruby","Python","Perl"]
 value.each{|v| puts v}
+
+{:name=>"yoshixmk",
+ :age=>22,
+ :length=>165,
+ :weight=>55}.each{|k,v|
+ p "<key:#{k}, value:#{v}>"
+}
+
+hash={:name=>"yoshixmk",
+ :age=>22,
+ :length=>165,
+ :weight=>55}
+hash2={:name=>"aaa",
+ :age=>20,
+ :length=>160,
+ :weight=>50}
+
+hash.each do |k,v|
+	 puts "#{k}, #{v}"
+end
+hash2.each do |k,v|
+	 puts "#{k}, #{v}"
+end
