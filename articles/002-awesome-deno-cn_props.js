488cd3edb3b5adbe2812585f7c0c596bb327ff71
yoshixmk <yoshixmk@users.noreply.github.com>
2020-06-14 15:50:39 +0000

    deploy: 8242a274b5b0b101ea66056028d05b6ab0eb0823

---
+++ articles/002-awesome-deno-cn_props.js
@@ -0,0 +1,134 @@
+import Ga from '/_ga.js';
+export default {
+    'config': {
+        "srcDir": ".",
+        "publicDir": "public",
+        "ignore": [
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+        ],
+        "base": "/",
+        "theme": "docs",
+        "plugins": [
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        ],
+        "watch": false,
+        "serve": false,
+        "port": 8000,
+        "title": "yoshixmk Profile",
+        "sidebar": [
+            "README.md",
+            {
+                "link": "articles/README.md",
+                "children": [
+                    "articles/001-install-and-hello-world.md",
+                    "articles/002-awesome-deno-cn.md"
+                ]
+            },
+            {
+                "link": "translations/README.md",
+                "children": [
+                    "translations/001-the-deno-handbook.md",
+                    "translations/002-deno-chat-app.md",
+                    "translations/003-from-node-to-deno.md"
+                ]
+            }
+        ],
+        "nav": [
+            {
+                "text": "Tegebu",
+                "link": "https://www.tegebu.com/"
+            },
+            {
+                "text": "Deno Third Party Modules Ranking",
+                "link": "https://deno.land/x/ranking"
+            },
+            {
+                "text": "Github",
+                "link": "https://github.com/yoshixmk"
+            },
+            {
+                "text": "Twitter",
+                "link": "https://twitter.com/yoshixmk"
+            },
+            {
+                "text": "Qiita",
+                "link": "https://qiita.com/yoshixmk"
+            }
+        ],
+        "ga": {
+            "id": "UA-149348992-2"
+        }
+    },
+    'pagePath': "articles/002-awesome-deno-cn.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "articles/002-awesome-deno-cn.html",
+    'title': "(2) TBD",
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<h1 id="(2)-tbd">(2) TBD <a class="header-anchor" href="#(2)-tbd">§</a></h1>\n<h2 id="tbd2">TBD2 <a class="header-anchor" href="#tbd2">§</a></h2>\n'
+        } }),
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/main.js", type: "module" })),
+    'sidebar': [
+        {
+            "text": "Profile",
+            "link": "index.html"
+        },
+        {
+            "link": "articles/index.html",
+            "children": [
+                {
+                    "text": "(1) TBD",
+                    "link": "articles/001-install-and-hello-world.html"
+                },
+                {
+                    "text": "(2) TBD",
+                    "link": "articles/002-awesome-deno-cn.html"
+                }
+            ],
+            "text": "ブログ"
+        },
+        {
+            "link": "translations/index.html",
+            "children": [
+                {
+                    "text": "TBD",
+                    "link": "translations/001-the-deno-handbook.html"
+                },
+                {
+                    "text": "TBD",
+                    "link": "translations/002-deno-chat-app.html"
+                },
+                {
+                    "text": "TBD",
+                    "link": "translations/003-from-node-to-deno.html"
+                }
+            ],
+            "text": "翻訳"
+        }
+    ],
+    'ga': React.createElement(Ga, { id: "UA-149348992-2" })
+};
---
+++ index_props.js
@@ -0,0 +1,134 @@
+import Ga from '/_ga.js';
+export default {
+    'config': {
+        "srcDir": ".",
+        "publicDir": "public",
+        "ignore": [
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}
+        ],
+        "base": "/",
+        "theme": "docs",
+        "plugins": [
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        ],
+        "watch": false,
+        "serve": false,
+        "port": 8000,
+        "title": "yoshixmk Profile",
+        "sidebar": [
+            "README.md",
+            {
+                "link": "articles/README.md",
+                "children": [
+                    "articles/001-install-and-hello-world.md",
+                    "articles/002-awesome-deno-cn.md"
+                ]
+            },
+            {
+                "link": "translations/README.md",
+                "children": [
+                    "translations/001-the-deno-handbook.md",
+                    "translations/002-deno-chat-app.md",
+                    "translations/003-from-node-to-deno.md"
+                ]
+            }
+        ],
+        "nav": [
+            {
+                "text": "Tegebu",
+                "link": "https://www.tegebu.com/"
+            },
+            {
+                "text": "Deno Third Party Modules Ranking",
+                "link": "https://deno.land/x/ranking"
+            },
+            {
+                "text": "Github",
+                "link": "https://github.com/yoshixmk"
+            },
+            {
+                "text": "Twitter",
+                "link": "https://twitter.com/yoshixmk"
+            },
+            {
+                "text": "Qiita",
+                "link": "https://qiita.com/yoshixmk"
+            }
+        ],
+        "ga": {
+            "id": "UA-149348992-2"
+        }
+    },
+    'pagePath': "README.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "index.html",
+    'title': "Profile",
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<h1 id="profile">Profile <a class="header-anchor" href="#profile">§</a></h1>\n<p>Top page</p>\n<h2 id="build">Build <a class="header-anchor" href="#build">§</a></h2>\n<pre class="language-autoit"><code class="language-autoit">$ deno run <span class="token operator">-</span><span class="token operator">-</span>unstable <span class="token operator">-</span><span class="token operator">-</span>allow<span class="token operator">-</span>read <span class="token operator">-</span><span class="token operator">-</span>allow<span class="token operator">-</span>write <span class="token operator">-</span><span class="token operator">-</span>allow<span class="token operator">-</span>net https<span class="token punctuation">:</span><span class="token operator">/</span><span class="token operator">/</span>deno<span class="token punctuation">.</span>land<span class="token operator">/</span>x<span class="token operator">/</span>pagic<span class="token variable">@0</span><span class="token punctuation">.</span><span class="token number">7.6</span><span class="token operator">/</span>mod<span class="token punctuation">.</span>ts build <span class="token operator">-</span><span class="token operator">-</span>serve <span class="token operator">-</span><span class="token operator">-</span>watch\n</code></pre>\n<h2 id="tbd">TBD <a class="header-anchor" href="#tbd">§</a></h2>\n'
+        } }),
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/main.js", type: "module" })),
+    'sidebar': [
+        {
+            "text": "Profile",
+            "link": "index.html"
+        },
+        {
+            "link": "articles/index.html",
+            "children": [
+                {
+                    "text": "(1) TBD",
+                    "link": "articles/001-install-and-hello-world.html"
+                },
+                {
+                    "text": "(2) TBD",
+                    "link": "articles/002-awesome-deno-cn.html"
+                }
+            ],
+            "text": "ブログ"
+        },
+        {
+            "link": "translations/index.html",
+            "children": [
+                {
+                    "text": "TBD",
+                    "link": "translations/001-the-deno-handbook.html"
+                },
+                {
+                    "text": "TBD",
+                    "link": "translations/002-deno-chat-app.html"
+                },
+                {
+                    "text": "TBD",
+                    "link": "translations/003-from-node-to-deno.html"
+                }
+            ],
+            "text": "翻訳"
+        }
+    ],
+    'ga': React.createElement(Ga, { id: "UA-149348992-2" })
+};
