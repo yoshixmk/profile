ab944805421c2aa6918a12b50d4f51e59b77af0a
yoshixmk <pirori67@gmail.com>
2020-06-27 21:13:21 +0900

    avoid ts error on ide

--- articles/004-deno-introduce-by-yoshixmk-regret-10-things.tsx
+++ articles/004-deno-introduce-by-yoshixmk-regret-10-things.tsx
@@ -1,11 +1,13 @@
 // @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
 import React from "https://dev.jspm.io/react@16.13.1";
 // @deno-types="https://github.com/svenanders/react-iframe/raw/v1.8.0/types.d.ts"
-import Iframe from 'https://cdn.pika.dev/react-iframe@1.8.0';
+import Iframe from "https://cdn.pika.dev/react-iframe@1.8.0";
 
+// @ts-ignore
+const headerLine = <h1>Deno 発表資料</h1>;
 const DenoIntro = () =>
   <>
-    <h1>Deno 発表資料</h1>
+    { headerLine }
     <Iframe
       src="https://docs.google.com/presentation/d/e/2PACX-1vR9fq06YxyGKCT6EqUBCSL_QzdkI7u3jb5NnA9IipzEAnyc50d6rqGxh6yOvgpY-3Fx9SmrGpMym-_P/embed?start=false&loop=false&delayms=3000"
       frameborder="0"
@@ -14,7 +16,8 @@ const DenoIntro = () =>
       allowfullscreen="true"
       mozallowfullscreen="true"
       webkitallowfullscreen="true"
-    ></Iframe>
+    >
+    </Iframe>
   </>;
 
 export default DenoIntro;
