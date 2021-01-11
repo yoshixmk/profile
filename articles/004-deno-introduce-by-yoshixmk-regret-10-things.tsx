3d6e7f27f005a6937477c3ade1566f51ad0f30fd
yoshixmk <pirori67@gmail.com>
2021-01-11 23:17:05 +0900

    fix: iframe

--- articles/004-deno-introduce-by-yoshixmk-regret-10-things.tsx
+++ articles/004-deno-introduce-by-yoshixmk-regret-10-things.tsx
@@ -1,23 +1,17 @@
 // @deno-types="https://cdn.pagic.org/@types/react@16.9.50/index.d.ts"
 import * as React from 'https://cdn.pagic.org/react@16.13.1/esnext/react.development.js';
-// @deno-types="https://github.com/svenanders/react-iframe/raw/v1.8.0/types.d.ts"
-import Iframe from "https://cdn.skypack.dev/react-iframe@1.8.0";
 
 // @ts-ignore
 const headerLine = <h1>Deno 発表資料</h1>;
 const DenoIntro = () =>
   <>
     {headerLine}
-    <Iframe
+    <iframe
       src="https://docs.google.com/presentation/d/e/2PACX-1vR9fq06YxyGKCT6EqUBCSL_QzdkI7u3jb5NnA9IipzEAnyc50d6rqGxh6yOvgpY-3Fx9SmrGpMym-_P/embed?start=false&loop=false&delayms=3000"
-      frameborder="0"
       width="700"
       height="500"
-      allowfullscreen="true"
-      mozallowfullscreen="true"
-      webkitallowfullscreen="true"
     >
-    </Iframe>
+    </iframe>
   </>;
 
 export default DenoIntro;
