403b4ab19d6d0aafaf76ca5c0689aacfdb39f597
Yoshihiro Ueki <pirori67@gmail.com>
2025-03-08 11:11:25 +0900

    add initial blog structure

--- articles/004-deno-introduce-by-yoshixmk-regret-10-things.tsx
+++ articles/004-deno-introduce-by-yoshixmk-regret-10-things.tsx
@@ -1,5 +1,5 @@
 // @deno-types="https://cdn.pagic.org/@types/react@16.9.50/index.d.ts"
-import * as React from 'https://cdn.pagic.org/react@16.13.1/esnext/react.development.js';
+// import * as React from 'https://cdn.pagic.org/react@16.13.1/esnext/react.development.js';
 
 // @ts-ignore
 const headerLine = <h1>Deno 発表資料</h1>;
